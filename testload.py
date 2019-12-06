977504f8efab652884036eaae9a6bda2033a82e6
ronthedistance <ron.revilla.30@my.csun.edu>
2019-12-06 15:50:42 -0800

    Update parallelization and notation on testload

--- testload.py
+++ testload.py
@@ -3,21 +3,38 @@ import multiprocessing as mp
 import time
 import http.client
 import socket
+
+"""
+The following code is used to stress test our site. Uses parallel HTTP connections to continuously attempt GETing the index page.
+"""
+
 class parallelRequestsLoader:
-    def getURL(self,url,port):
+    def getURL(self,url):
+        port=80
         connection = http.client.HTTPConnection(url, port,timeout=5)
         connection.request('GET', url)
-        #is head fine to use? will save some time compared to get
         response = connection.getresponse()
-        #find prevents us from needing to catch errors. we don't need to find or index the string "disney" we simply need to know if it exists.
         print("Server response: " + str(response.status))
         #list objects for later usage with comparison.
         connection.close()
 loader = parallelRequestsLoader()
-loader.getURL("http://demo-eu-alb-1939214429.eu-west-2.elb.amazonaws.com/",80)
+#instantantiate an object of class RequestsLoader
+debugLevel = 0
+lazyList = []
+numberOfRequests = 10000
+#how many requests we want to make at once
+start = 0
+#can be changed to print out response
+if debugLevel == 1:
+    loader.getURL("SeniorDesignProject2ALB-790864702.us-west-2.elb.amazonaws.com")
+
+while start < numberOfRequests:
+    lazyList.append("SeniorDesignProject2ALB-790864702.us-west-2.elb.amazonaws.com")
+    print("%d of %d" % (start,numberOfRequests))
+    start = start+1
+#generate a list of 10,000 of the same IP to be used for multiprocessing
 start_time = time.time()
 with PoolExecutor(max_workers=20) as executor:
-    for _ in executor.map(loader.getURL("http://demo-eu-alb-1939214429.eu-west-2.elb.amazonaws.com/",80)
-,100000):
+    for _ in executor.map(loader.getURL,lazyList):
         pass
 print("--- %s seconds ---" % (time.time() - start_time))    
